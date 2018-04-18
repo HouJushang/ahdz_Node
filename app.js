67e7d5517b34ccc01a6c8b6e8f92aabbb76e6d44
后居上 <hou@houjushangmacpro.local>
2018-04-18 10:01:54 +0800

    111

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const app = require('./bin/app')
 const router = require('./module/router');
 const session = require('./bin/session')
 const config = require('./config/www')
-const bodyParser = require('koa-bodyparser');
+const koaBody = require('koa-body');
 const cors = require('koa2-cors');
+const serve = require('koa-static');
 const co = require('co');
 const path = require('path')
 const render = require('koa-swig');
+const ueditor = require('koa2-ueditor')
 require('./util/globalFunction')
-require('./module');
 
-
+app.use(serve(path.join(__dirname, '/public')));
 app.context.render = co.wrap(render({
     root: path.join(__dirname, 'views'),
     autoescape: true,
@@ -21,9 +22,15 @@ app.context.render = co.wrap(render({
 
 app.use(cors());
 
+require('./module');
+
 app.keys = ['ahdzsecret'];
 app.use(session.module(session.config, app));
-app.use(bodyParser());
+app.use(koaBody({ multipart: true }));
+
+
 app.use(router.routes()).use(router.allowedMethods());
+
+
 
 app.listen(config.port);
