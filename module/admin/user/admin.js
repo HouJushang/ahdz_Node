0b82a2c189e500b6dae73e3c7d81a3b8aab7f1d1
后居上 <5466315@qq.com>
2018-05-15 02:53:26 +0800

    11

--- module/admin/user/admin.js
+++ module/admin/user/admin.js
@@ -2,7 +2,7 @@
  * Created by hou on 2018/4/15.
  */
 const router = require('../../router');
-const Sequelize = require('Sequelize')
+const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 const adminModel = _loadModel('userPermission', 'admin');
 const defaultUrl = '/admin/admin';
