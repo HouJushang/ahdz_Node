15c53a7b558c8ff31c1a75543c269f001abcf908
后居上 <5466315@qq.com>
2018-05-16 23:41:20 +0800

    111

--- model/userPermission/roleMenu.js
+++ model/userPermission/roleMenu.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 const sequelize = _loadSequelize();
+const menuModel = _loadModel('userPermission', 'menu');
+const roleModel = _loadModel('userPermission', 'role');
 const RoleMenu = sequelize.define('role_menu', {
     id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-    roleId: { type: Sequelize.INTEGER, allowNull: false },
-    menuId: { type: Sequelize.INTEGER, allowNull: false },
 });
+RoleMenu.belongsTo(menuModel);
+RoleMenu.belongsTo(roleModel);
 module.exports = RoleMenu;
--- module/admin/user/menu.js
+++ module/admin/user/menu.js
@@ -1,9 +1,30 @@
 const router = require('../../router');
+const roleMenuModel = _loadModel('userPermission', 'roleMenu')
 const menuModel = _loadModel('userPermission', 'menu');
+const roleModel = _loadModel('userPermission', 'role');
+
 router.get('/admin/menu', async (ctx) => {
     try {
+        console.log(ctx.session.loginInfo.adminRoleResult);
         const result = await menuModel.findAll();
         ctx.body = _successResponse('菜单列表获取成功', result);
+    } catch (e) {
+        ctx.body = _errorResponse(e.message)
+    }
+})
+router.get('/admin/shoRoleMenu', async (ctx) => {
+    try {
+        const result = await roleMenuModel.findAll({
+            where:{
+                roleId: ctx.session.loginInfo.adminRoleResult.roleId
+            },
+            include: [roleModel, menuModel]
+        });
+        const data = []
+        result.forEach(e => {
+            data.push(e.menu)
+        })
+        ctx.body = _successResponse('菜单列表获取成功', data);
     } catch (e) {
         ctx.body = _errorResponse(e.message)
     }
